25b7606d5a0ff00a04c5a494ea069accc56b036a
eujohnio <eujohnio@gmail.com>
2021-03-04 01:39:38 +0300

    Add commission, tax to profit calculating

--- config.go
+++ config.go
@@ -8,16 +8,20 @@ import (
 )
 
 type config struct {
-	MoexAuthentication string `yaml:"moexAuthentication"`
-	MoexPrefixHttps    string `yaml:"moexPrefixHttps"`
-	Password           string `yaml:"password"`
-	UserName           string `yaml:"userName"`
-	RequestInterval    int16  `yaml:"requestInterval"`
-	TlgChatId          int64  `yaml:"tlgChatId"`
-	TlgAccessToken     string `yaml:"tlgAccessToken"`
+	MoexAuthentication string  `yaml:"moexAuthentication"`
+	MoexPrefixHttps    string  `yaml:"moexPrefixHttps"`
+	Password           string  `yaml:"password"`
+	UserName           string  `yaml:"userName"`
+	RequestInterval    int16   `yaml:"requestInterval"`
+	TlgChatId          int64   `yaml:"tlgChatId"`
+	TlgAccessToken     string  `yaml:"tlgAccessToken"`
+	Commission         float32 `yaml:"commission"`
+	Tax                float32 `yaml:"tax"`
+	MinProfit          float64 `yaml:"minProfit"`
 	Shares             []struct {
 		Ticker     string  `yaml:"ticker"`
 		Startprice float64 `yaml:"startprice"`
+		Number     int16   `yaml:"number"`
 	} `yaml:"shares"`
 }
 
--- main.go
+++ main.go
@@ -13,13 +13,21 @@ func backgroundTask() {
 		for _, share := range c.Shares {
 			a := getCurrentPrice(share.Ticker, *c)
 			profit := (a / share.Startprice * 100) - 100
-			if profit > 5 {
+			if profit > float64(2*c.Commission) {
 				profitRate := math.Round(profit*100) / 100
+				moneyProfit := calcProfit(share.Number, share.Startprice, a, *c)
+				if moneyProfit > c.MinProfit {
+					sendMessage(share.Ticker+": +"+strconv.FormatFloat(profitRate, 'f', 2, 64)+"%, "+strconv.FormatFloat(moneyProfit, 'f', 2, 64), *c)
+				}
 				//fmt.Println(share.Ticker, math.Round(profit*100)/100)
-				sendMessage(share.Ticker+": +"+strconv.FormatFloat(profitRate, 'f', 2, 64)+"%", *c)
 			}
 		}
 	}
+}
+
+func calcProfit(number int16, startprice, sellprice float64, c config) float64 {
+	profit := float64(number) * ((sellprice-startprice)*float64(100-c.Tax)/100 - (sellprice+startprice)*float64(c.Commission)/100)
+	return profit
 }
 
 func main() {
